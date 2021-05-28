f3c0419ccc56a949f287a2338d275fecd930543a
Tex Tang <tex_tang@outlook.com>
2021-05-28 21:20:13 +0800

    Update trend-2 symbol

--- config/trend-2.js
+++ config/trend-2.js
@@ -2,11 +2,11 @@ const moment = require("moment");
 
 module.exports = {
   name: "TrendStrategy2",
-  symbols: ["XRPUSDT", "MBLUSDT", "ONGUSDT", "BLZUSDT", "PERPUSDT"],
+  symbols: ["XRPUSDT", "MBLUSDT", "BLZUSDT", "PERPUSDT"],
 
   baseAsset: "USDT",
   startingBalance: 60,
-  maxAllowedOpenTrades: 5,
+  maxAllowedOpenTrades: 4,
 
   startTime: moment("20210501").utc().valueOf(),
   endTime: moment("20210522").utc().valueOf(),
